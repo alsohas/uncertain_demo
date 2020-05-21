008f01b3a80fa825a06d8ee59689b25a2e6a70ff
Abdullah <aislam96@uw.edu>
2020-05-20 18:43:57 -0700

    fixed obsoletes not drawing + nth step prediction

--- node/src/Forest/core/PredictiveForest.ts
+++ node/src/Forest/core/PredictiveForest.ts
@@ -3,7 +3,7 @@ import { Region, } from './Region';
 import * as L from 'leaflet';
 import { getNumPredictiveSteps, } from '../../map_utils/DomUtils';
 import { LayerManger, } from '../../map_utils/LayerManager';
-import { intersect, union, } from '../util/setops';
+import { intersect, union, difference, } from '../util/setops';
 import { RegionalNode, } from './RegionalNode';
 import { PredictiveNode, } from './PredictiveNode';
 import { NTNode, } from '../util/ntnode';
@@ -18,32 +18,57 @@ export class PredictiveForest {
   PredictiveEdgeMarkers: Array<L.Polyline> = [];
   ObsoleteMarkers: Array<L.Polyline> = [];
 
+  /**
+   * Creates an instance of PredictiveForest.
+   * @param {L.Map} `Leaflet.Map` object to be hooked into for GUI manipulation
+   * @memberof PredictiveForest
+   */
   constructor(mMap: L.Map) {
     this.mMap = mMap;
     this.RoadNetwork = RoadNetwork.getInstance();
     this.Region = Region.getInstance();
     this.ValidEdgeMarkers = new Map();
-    (document.getElementById('showPredictive') as HTMLElement).addEventListener('click', (e) => {
-      if (((e.target) as HTMLInputElement).checked) {
-        this.PredictiveEdgeMarkers.map(e => e.addTo(mMap));
-      } else {
-        this.PredictiveEdgeMarkers.map(e => e.removeFrom(mMap));
+    (document.getElementById('showPredictive') as HTMLElement).addEventListener(
+      'click',
+      (e) => {
+        if ((e.target as HTMLInputElement).checked) {
+          this.PredictiveEdgeMarkers.map((e) => e.addTo(mMap));
+        } else {
+          this.PredictiveEdgeMarkers.map((e) => e.removeFrom(mMap));
+        }
       }
-    });
-    (document.getElementById('showObsolete') as HTMLElement).addEventListener('click', (e) => {
-      if (((e.target) as HTMLInputElement).checked) {
-        this.ObsoleteMarkers.map(e => e.addTo(mMap));
-      } else {
-        this.ObsoleteMarkers.map(e => e.removeFrom(mMap));
+    );
+    (document.getElementById('showObsolete') as HTMLElement).addEventListener(
+      'click',
+      (e) => {
+        if ((e.target as HTMLInputElement).checked) {
+          this.ObsoleteMarkers.map((e) => e.addTo(mMap));
+        } else {
+          this.ObsoleteMarkers.map((e) => e.removeFrom(mMap));
+        }
       }
-    });
+    );
   }
 
+  /**
+   *
+   * Computed value retrieves the depth of predictive trees from user input
+   * @readonly
+   * @type {number}
+   * @memberof PredictiveForest
+   */
   public get Depth(): number {
     return parseInt(getNumPredictiveSteps());
   }
 
 
+  /**
+   *
+   * Updates the predictive forest by building a new region around the given center
+   * @param {L.LatLng} center Central point of new region
+   * @returns {Promise<void>} void
+   * @memberof PredictiveForest
+   */
   public async Update(center: L.LatLng): Promise<void> {
     if (this.CurrentStep === 0) {
       const nodeIDs = await RoadNetwork.getNodesWithinRange(center);
@@ -104,24 +129,64 @@ export class PredictiveForest {
     this.drawEdges();
     this.CurrentStep++;
   }
+
+  /**
+   * Marks all obsolete nodes on the map
+   *
+   * @private
+   * @memberof PredictiveForest
+   */
   private markObsoletes(): void {
+    const globalObsoletes = this.getGlobalObsoletes();
     LayerManger.getInstance(this.mMap).markObsoleteNodes(
-      this.Region.ObsoleteNodes, this.ObsoleteMarkers,
+      globalObsoletes,
+      this.ObsoleteMarkers
     );
     this.ValidEdgeMarkers.forEach((polyLine, idArray) => {
-      if (
-        this.Region.ObsoleteNodes.has(idArray[0]) ||
-        this.Region.ObsoleteNodes.has(idArray[1])
-      ) {
+      if (globalObsoletes.has(idArray[0]) || globalObsoletes.has(idArray[1])) {
         polyLine.setStyle({
-          color: '#E58E35',
-          fillColor: '#E58E35',
+          // color: '#E58E35',
+          // fillColor: '#E58E35',
+          color: 'red',
+          fillColor: 'red',
+          opacity: 0.5,
+          weight: 8,
         });
         this.ObsoleteMarkers.push(polyLine);
       }
     });
   }
 
+
+  /**
+   * Retrieves nodes that have no references to any existing region
+   *
+   * @private
+   * @returns {Set<number>} Globally obsolete nodes
+   * @memberof PredictiveForest
+   */
+  private getGlobalObsoletes(): Set<number> {
+    let globalObsoletes = new Set<number>();
+    this.Region.ObsoleteNodes.forEach((obsoleteNodes) => {
+      globalObsoletes = union(globalObsoletes, obsoleteNodes);
+    });
+    console.log(globalObsoletes);
+    let globalValids = new Set<number>();
+    this.Region.Regions.forEach((region) => {
+      const nodeIDs = new Set(region.keys());
+      globalValids = union(globalValids, nodeIDs);
+    });
+    globalObsoletes = difference(globalObsoletes, globalValids);
+    return globalObsoletes;
+  }
+
+
+  /**
+   * Draws polylines representing edges
+   *
+   * @private
+   * @memberof PredictiveForest
+   */
   private drawEdges(): void {
     const currentRegion = this.Region.Regions.get(this.CurrentStep) as Map<
       number,
@@ -143,6 +208,14 @@ export class PredictiveForest {
     });
   }
 
+  /**
+   * Starts the expansion of predictive trees from the latest region
+   *
+   * @private
+   * @param {Map<number, RegionalNode>} newRegion Nodes within a region
+   * @returns {Promise<void>} void
+   * @memberof PredictiveForest
+   */
   private async ExpandPredictiveTrees(
     newRegion: Map<number, RegionalNode>
   ): Promise<void> {
@@ -169,17 +242,23 @@ export class PredictiveForest {
     steps: number,
     obsoleteNodes: Set<number>
   ): Promise<void> {
-    this.Region.ObsoleteNodes = union(this.Region.ObsoleteNodes, obsoleteNodes);
     if (obsoleteNodes.size === 0) {
       return;
     }
-    if (steps <= 1) {
-      return;
+    const _obsoletesNodes = this.Region.ObsoleteNodes.get(steps);
+    if (_obsoletesNodes) {
+      this.Region.ObsoleteNodes.set(
+        steps,
+        union(_obsoletesNodes, obsoleteNodes)
+      );
+    } else {
+      this.Region.ObsoleteNodes.set(steps, obsoleteNodes);
     }
+
     const obsoleteParents = new Set<number>();
     const region = this.Region.Regions.get(steps);
     const parentalRegion = this.Region.Regions.get(steps - 1);
-    if (!parentalRegion || !region) {
+    if (!region) {
       return;
     }
     obsoleteNodes.forEach((nodeID) => {
@@ -190,13 +269,14 @@ export class PredictiveForest {
         return;
       }
       parents.forEach((pNodeID) => {
-        const parent = parentalRegion.get(pNodeID);
+        const parent = parentalRegion?.get(pNodeID);
         parent?.Children.delete(nodeID);
         if (parent?.Children.size === 0) {
           obsoleteParents.add(pNodeID);
         }
       });
     });
+
     this.PruneRegions(steps - 1, obsoleteParents);
   }
 
